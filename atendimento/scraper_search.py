59c6bb2c5b9d402daef3e3e2426ff465f3f07678
gutobiel <gutobielsantos@gmail.com>
2026-06-19 16:31:37 -0300

    feat: implement automated payment scraper and testing utilities for HC Pneus site

--- atendimento/scraper_search.py
+++ atendimento/scraper_search.py
@@ -52,6 +52,7 @@ def buscar_pneus_no_site(car_brand, car_model, car_year, car_version, rim_size):
         chrome_options.add_argument("--disable-software-rasterizer")
         chrome_options.add_argument("--disable-extensions")
         chrome_options.add_argument("--disable-setuid-sandbox")
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
         chrome_options.add_experimental_option("prefs", {"profile.managed_default_content_settings.images": 2})
     chrome_options.add_argument("--window-size=1280,800")
     chrome_options.add_argument("--disable-blink-features=AutomationControlled")
